8f21b11431fa78f641ae3d1688bad8b08f3cdcd5
atillatoken-star <atillatoken@gmail.com>
2026-08-13 23:31:13 +0300

    Create headers.py

---
+++ modules/headers.py
@@ -0,0 +1,13 @@
+import requests
+
+def get_headers(url):
+    try:
+        response = requests.get(url, timeout=10)
+
+        return {
+            "status_code": response.status_code,
+            "headers": dict(response.headers),
+        }
+
+    except Exception as e:
+        return {"error": str(e)}
