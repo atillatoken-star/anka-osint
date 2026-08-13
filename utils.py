465c2f4521470b774c5dce6589346718cfd0d895
atillatoken-star <atillatoken@gmail.com>
2026-08-13 23:17:44 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-
-from urllib.parse import urlparse
-
-def normalize_target(target: str) -> str:
-    if target.startswith("http://") or target.startswith("https://"):
-        return urlparse(target).netloc
-    return target
