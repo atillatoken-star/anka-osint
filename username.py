2d96ddb979c856ed70ea1f456fa83a45f009b1f9
atillatoken-star <atillatoken@gmail.com>
2026-08-13 23:17:24 +0300

    Delete username.py

--- username.py
+++
@@ -1,8 +0,0 @@
-
-def analyze_username(username):
-    return {
-        "target": username,
-        "length": len(username),
-        "has_number": any(c.isdigit() for c in username),
-        "has_underscore": "_" in username
-    }
