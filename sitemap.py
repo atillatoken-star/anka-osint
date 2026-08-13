79dd04e8e376762c85b32a96d5d201196681808f
atillatoken-star <atillatoken@gmail.com>
2026-08-13 23:16:54 +0300

    Delete sitemap.py

--- sitemap.py
+++
@@ -1,8 +0,0 @@
-
-import requests
-
-def fetch_sitemap(url):
-    try:
-        return requests.get(url.rstrip("/") + "/sitemap.xml", timeout=10).text
-    except Exception as e:
-        return str(e)
