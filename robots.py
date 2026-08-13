58d27c37902c14126ac3923068ac17bed0abad84
atillatoken-star <atillatoken@gmail.com>
2026-08-13 23:16:45 +0300

    Delete robots.py

--- robots.py
+++
@@ -1,8 +0,0 @@
-
-import requests
-
-def fetch_robots(url):
-    try:
-        return requests.get(url.rstrip("/") + "/robots.txt", timeout=10).text
-    except Exception as e:
-        return str(e)
