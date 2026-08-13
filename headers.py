e5233b99c6b17799afef32b40e8bb58dd3dddc8f
atillatoken-star <atillatoken@gmail.com>
2026-08-13 23:15:48 +0300

    Delete headers.py

--- headers.py
+++
@@ -1,9 +0,0 @@
-
-import requests
-
-def fetch_headers(url):
-    try:
-        r = requests.get(url, timeout=10)
-        return dict(r.headers)
-    except Exception as e:
-        return {"error": str(e)}
